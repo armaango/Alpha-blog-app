867fa829be622a282d60c0c6d6c66228f172ad5f
armaango <armaan.goyal@gmail.com>
2016-12-19 19:59:34 -0500

    Remove redundancy and get code production ready

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-
+  before_action :set_article, only:[:edit, :show, :destroy, :update]
   def index
     @articles = Article.all
 
@@ -23,35 +23,33 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def update
-    @article = Article.find(params[:id])
     if @article.update(article_params)
       flash[:notice] = "The article was updated successfully"
       redirect_to article_path(@article)
     else
       render 'edit'
     end
-
   end
 
   def show
-    @article = Article.find(params[:id])
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     flash[:notice] = "Article deleted successfully"
     redirect_to articles_path
   end
 
   private
-  def article_params
-    params.require(:article).permit(:title, :description)
+    def set_article
+      @article = Article.find(params[:id])
+    end
 
-  end
+    def article_params
+      params.require(:article).permit(:title, :description)
+    end
 
 end
