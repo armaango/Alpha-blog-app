b7900d6b235110f27eef408e12ca6329379a2f25
armaango <armaan.goyal@gmail.com>
2016-12-23 23:34:42 -0500

    Added tests and categories

--- config/routes.rb
+++ config/routes.rb
@@ -15,6 +15,8 @@ Rails.application.routes.draw do
 
   resources :users, except: [:new]
 
+  resources :categories, except: [:destroy]
+
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
